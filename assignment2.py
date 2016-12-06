27b53a4bec29094a840603bc722c16fd2f3d251a
Jack Han <jcdhan1@sheffield.ac.uk>
2016-12-06 06:27:09 +0000

    Dimensionality Reduction and Noise Robustness

--- assignment2.py
+++ assignment2.py
@@ -1,8 +1,12 @@
+"""Jack Cheng Ding Han
+  150159519
+  ACA15JCH"""
 import pickle
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import numpy as np
 import types
+from scipy import linalg
 
 #Pre-processing
 GRID_LENGTH=15
@@ -14,6 +18,8 @@ train_labels = data['train_labels']
 test1 = data['test1']
 test2 = data['test2']
 words  = data['words']
+
+#What letters the classifer gave to each block in test1 or test2 will be compared to this.
 TEST_LABELS = np.array([14,18,25, 5,12, 7,14, 1,12,14,12, 5,16, 2,18,
 						21,14,15,14, 6, 8, 2,18,15,16, 5,18,18,13,16,
 						 3,15,14, 2,20, 8, 7, 9,18,23,25, 2,14, 1, 6,
@@ -30,7 +36,15 @@ TEST_LABELS = np.array([14,18,25, 5,12, 7,14, 1,12,14,12, 5,16, 2,18,
 						14, 2,18,20,18,19,12,14, 5, 1, 8, 1, 7, 8,14,
 						 1,12, 5,21,20,14, 5,11,23,14, 9,12, 5,19,12])
 
+#The start and end points of each line drawn by wordsearch in each trial will be compared to this.
+CORRECT_LINES = [(192,132),(180, 68),(154, 42),( 92, 32),( 30, 75),( 55, 51),
+				 (186,184),( 63,111),( 44,156),(131,191),(165, 90),( 74,172),
+				 (217,214),(  8,  2),(219,107),( 29, 85),( 89,164),(151,106),
+				 ( 91, 16),(  1,113),( 22, 26),(223,103),(59, 157),( 39, 34)]
+
 def get_name(testN):
+	"""Returns the name of the test data as a string.
+	   testN: test1 or test2"""
 	return "test1" if np.ndarray.tolist(test1)==np.ndarray.tolist(testN) else "test2"
 
 def get_characters(ndimarray, as_vectors):
@@ -73,6 +87,10 @@ def classify(train, train_labels, test):
 	return labels
 
 def correctly_processed(classified,actual,percentage):
+	"""Compares the results of the classifier to the actual answer
+		classified: the data that's already been classified
+			actual: the actual answer
+		percentage: if this is True, a percentage is returned."""
 	correct=0
 	for i in range(0,classified.shape[0]):
 		if (classified[i]==actual[i]):
@@ -103,7 +121,7 @@ def search(list_to_search,word,direction='A'):
 			if num_correct>0:
 				forward+=[(orig_indices[pos],orig_indices[pos+len(word)-1],num_correct)]
 			elif num_correct==len(word):
-				forward==[(orig_indices[pos],orig_indices[pos+len(word)-1],num_correct)]
+				return [(orig_indices[pos],orig_indices[pos+len(word)-1],num_correct)]
 			pos+=1
 		return forward
 	elif (direction=='B'):
@@ -162,17 +180,37 @@ def draw_line(graph, start, end, side_length):
 		plt.ylim(GRID_LENGTH*side_length, 0)
 		graph.plot(xSxE, ySyE, 'y-')
 
-def wordsearch(testN, words_to_find, train_dat, train_lbl):
-	print("Solving " + get_name(testN))
+#Dimensionality Reduction
+def reduce(test_dat,train_dat,reduce_by,as_vectors=False):
+	""" test_data: Testing data to reduce
+	   train_data: Training data to use
+		reduce_by: The number of components at the end
+	   as_vectors: If true it returns an array of vectors with dimensions equal to reduce_by, in false,
+				   The number dimensions of the output are the same as that of test_dat, allowing it to be plotted."""
+	covx = np.cov(train_data, rowvar=0)
+	N = covx.shape[0]
+	w, v = linalg.eigh(covx, eigvals=(N-reduce_by, N-1))
+	v = np.fliplr(v)
+	if as_vectors:
+		return np.dot((test_dat - np.mean(train_dat)), v)
+	else:
+		return np.dot(np.dot((test_dat - np.mean(train_dat)), v), v.transpose()) + np.mean(train_dat)
+
+#Loop over all words
+def wordsearch(testN, words_to_find, train_dat, train_lbl,reduced=False):
+	print("Solving " + get_name(testN) + (" With Reduction" if reduced else ""))
 	plt.matshow(testN, cmap=cm.gray)
-	#Classification, Pre-processing
-	classified_mat = classify(train_dat, train_lbl, get_characters(testN, True))
+	#Classification, Pre-processing, Dimensionality Reduction
+	training_dat = reduce(train_dat,train_dat,10,True) if reduced else train_dat
+	preprocessed = get_characters(testN, True)
+	classified_mat = classify(training_dat, train_lbl, reduce(preprocessed,train_dat,10,True) if reduced else preprocessed)
 	print(str(correctly_processed(classified_mat,TEST_LABELS,False))
 	  + " letters were correctly labeled which is about "
 	  + str(correctly_processed(classified_mat,TEST_LABELS,True)) + "% of the letters.")
 	#Give every element a unique number because the functions to be used such as diagonal do not preserve indices
 	indices_grid=np.reshape(range(classified_mat.shape[0]), (GRID_LENGTH,GRID_LENGTH))
 	#Select a word to find
+	lines=[]
 	for w_string in words_to_find:
 		w=[ord(ch)-96 for ch in list(w_string)]
 		#Perform the search
@@ -186,17 +224,25 @@ def wordsearch(testN, words_to_find, train_dat, train_lbl):
 			lists_searched+=search(index_label_list(classified_mat,indices_grid[:,c]),w)
 		#Search all diagonals that are longer than or just as long as the word.
 			lists_searched=trav_diag(classified_mat,indices_grid,lists_searched,w)
-
 		#Likeliest is the tuple that has the biggest third element
 		likeliest=sorted(lists_searched,key=lambda trituple: trituple[2])[len(lists_searched)-1]
-		#Return the indices to start and end the line from.
+		#Return starting index and ending index for this word.
 		likeliest_tuple=likeliest[:2]
+		lines+=[likeliest_tuple]
 		#Display the result
 		print("Line from index " + str(likeliest_tuple[0]) + " to index " + str(likeliest_tuple[1]))
 		draw_line(plt,int(likeliest_tuple[0]), int(likeliest_tuple[1]), BLOCK_LENGTH)
 	print("Solved")
-	plt.gcf().savefig(get_name(testN), dpi=100)
+	print(str(sum([t[0] and t[1] for t in (np.array(CORRECT_LINES)==(np.array(lines)))])) + " out of " + str(len(words)) + " found correctly.")
+	plt.gcf().savefig(get_name(testN) + ("_reduced" if reduced else ""), dpi=100)
 
-
+#Searching test1 with 900 features.
 wordsearch(test1, words, train_data, train_labels)
+#Searching test1 with reduction to 10 features.
+wordsearch(test1, words, train_data, train_labels,True)
+
+#Noise Robustness
+#Searching test2 with 900 features.
 wordsearch(test2, words, train_data, train_labels)
+#Searching test2, with reduction to 10 features.
+wordsearch(test2, words, train_data, train_labels,True)
